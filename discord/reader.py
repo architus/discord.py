f3e18854d5020a26580a16874d3366c6856c0a48
James Johnson <jjohnson474@gatech.edu>
2020-06-01 01:19:40 +0000

    let's find out what this tuple is

--- discord/reader.py
+++ discord/reader.py
@@ -122,7 +122,7 @@ class TCPSink(AudioSink):
                      'header', 'data', 'decrypted_data', 'extension')
         """
         data = bytearray(13)
-        print(f"UID: {type(uid)}")
+        print(f"UID: {uid}")
         size = len(packet.decrypted_data)
         struct.pack_into(">BHQH", data, 0, 0x00, size, uid, packet.sequence)
         for byte in packet.decrypted_data:
